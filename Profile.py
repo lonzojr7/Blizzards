70b8dcaf315944250438480ccb822825b7b28dfd
Robin <robinhallct@gmail.com>
2025-11-18 22:47:14 -0600

    Add __repr__ method to Profile

--- Profile.py
+++ Profile.py
@@ -11,3 +11,6 @@ class Profile:
 
     def update_schedule(self, new_schedule):
         self.schedule = new_schedule
+
+    def __repr__(self):
+        return f"Profile(name={self.name}, major={self.major}, minor={self.minor}, schedule={self.schedule})"
